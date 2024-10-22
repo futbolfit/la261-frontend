9c1efdd96120034df873661ea5c1cd16dcf0864c
Angel <angel.rodrigues@satschel.com>
2024-10-22 12:41:06 -0500

    feat: underline solid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CommonModule
-  ],
+    CommonModule,
+    NavbarComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'la261';
-  isSidebarOpen: boolean = false;
 
-  handleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen;
-  }
 }
